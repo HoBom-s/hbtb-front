fbb59d6dbef438a4ba35a5a9cde450ad504cf801
FoxMon <jjockrod@naver.com>
2023-05-22 13:09:13 +0900

    name static const

--- src/static/name.js
+++ src/static/name.js
@@ -1,5 +1,14 @@
+/**
+ * NameSpace에 대한 Static Const 정의
+ */
 const namespace = {
+  // Type Agent에 대한 Const
+  types: "Types",
+
+  // Tag Schema에 대한 Const
   tagSchema: "TagSchema",
+
+  // Category Schema에 대한 Const
   categorySchema: "CategorySchema",
 };
 
