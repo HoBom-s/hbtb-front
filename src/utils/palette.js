1c11b80ecc5f0a7c12deed33305560a8d8729c6e
foxmon <jjockrod@naver.com>
2023-06-12 22:48:28 +0900

    palette

---
+++ src/utils/palette.js
@@ -0,0 +1,23 @@
+const palette = {
+  colors: {
+    mainBlack: "#08090A",
+    mainOrange: "#FFA630",
+    mainWhite: "#FAFAFF",
+    chipGray: "#b0bec5",
+  },
+  fontWeight: {
+    thin: 200,
+    regular: 400,
+    bold: 700,
+  },
+  fontSize: {
+    xs: "12px",
+    s: "14px",
+    md: "16px",
+    l: "18px",
+    xl: "20px",
+  },
+};
+
+Object.freeze(palette);
+export default palette;
