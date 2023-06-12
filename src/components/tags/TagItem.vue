ffe9ee780442544e8946a0baa478d2ee96cd266e
foxmon <jjockrod@naver.com>
2023-06-12 10:14:11 +0900

    hbtb tag component

---
+++ src/components/tags/TagItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <q-chip color="blue-grey-3" @click="onTagItemClickEvent(props.tag)">
+      {{ props.tag.title }}
+    </q-chip>
+  </div>
+</template>
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  tag: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  onTagItemClickEvent: () => true,
+});
+
+// Methods
+function onTagItemClickEvent(clickedTag) {
+  emits("onTagItemClickEvent", clickedTag);
+}
+</script>
