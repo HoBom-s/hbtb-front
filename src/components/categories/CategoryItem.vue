9010d41cfe5251401dc947455573ca37138d394e
foxmon <jjockrod@naver.com>
2023-06-12 22:48:59 +0900

    Feat: CategoryItem component

---
+++ src/components/categories/CategoryItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <span
+      :style="categoryStyle"
+      @click="onCategoryItemClickEvent(props.category)"
+    >
+      {{ props.category.title }}
+    </span>
+  </div>
+</template>
+<script setup>
+import { defineProps, defineEmits, computed } from "vue";
+
+import palette from "@/utils/palette";
+
+const props = defineProps({
+  category: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  onCategoryItemClickEvent: () => true,
+});
+
+const categoryStyle = computed(() => {
+  return {
+    margin: "0 auto",
+    fontSize: palette.fontSize.l,
+    fontWeight: palette.fontWeight.bold,
+    borderBottom: `3px solid ${palette.colors.mainOrange}`,
+  };
+});
+
+function onCategoryItemClickEvent(clickedCategory) {
+  emits("onCategoryItemClickEvent", clickedCategory);
+}
+</script>
