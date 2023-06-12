ff316a1ce398a79e45d250bef8511a78a3668eae
foxmon <jjockrod@naver.com>
2023-06-12 22:49:11 +0900

    Feat: CategoryItemList component

---
+++ src/components/categories/CategoryItemList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <CategoryItem
+      v-for="category of props.categories"
+      :key="category._id"
+      :category="category"
+      @onCategoryItemClickEvent="onCategoryItemClickEvent"
+    />
+  </div>
+</template>
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+import CategoryItem from "./CategoryItem.vue";
+
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  onCategoryItemClickEvent: () => true,
+});
+
+function onCategoryItemClickEvent(clickedCategory) {
+  emits("onCategoryItemClickEvent", clickedCategory);
+}
+</script>
