937a7539fedd4e83bbe7b37497f81900c6c9129f
foxmon <jjockrod@naver.com>
2023-06-13 10:57:53 +0900

    Feat: MainTechPage

---
+++ src/pages/MainTechPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <TagItemList :tags="state.tags" />
+</template>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+
+import TagItemList from "@/components/tags/TagItemList.vue";
+
+import { getAllTagRequestService } from "@/apis/tagFetcher";
+
+import { agent } from "@/types";
+
+import namespace from "@/static/name";
+
+const state = reactive({
+  tags: [],
+});
+
+onMounted(async () => {
+  const tags = await getAllTagRequestService();
+  const tagInstanceArray = tags.map((tag) => {
+    const { _id, title, path, createdAt, count } = tag;
+    const tagObj = agent
+      .instanceOfName(namespace.tagSchema)
+      .createInstance(_id, title, path, count, createdAt);
+    return tagObj;
+  });
+  if (tagInstanceArray.every((tag) => tag.ofPathCondition(tag.path))) {
+    state.tags = tagInstanceArray;
+  } else {
+    state.tags = [];
+  }
+});
+</script>
