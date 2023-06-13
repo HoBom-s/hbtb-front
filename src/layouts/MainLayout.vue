a26fa84cb78fb7f361efbdf43d2e764b0b321fca
foxmon <jjockrod@naver.com>
2023-06-13 10:57:21 +0900

    Fix: MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,52 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <AppHeader />
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
+    <AppHeader
+      :categories="state.categories"
+      :selectedCategory="state.selectedCategory"
+      @onCategoryItemClickEvent="onCategoryItemClickEvent"
+    />
+    <router-view />
     <AppFooter />
   </q-layout>
 </template>
-;
 
 <script setup>
-import AppHeader from "../components/AppHeader.vue";
-import AppFooter from "../components/AppFooter.vue";
+import { reactive, onMounted } from "vue";
+
+import AppHeader from "../components/common/AppHeader.vue";
+import AppFooter from "../components/common/AppFooter.vue";
+
+import { getAllCategoryRequestService } from "@/apis/categoryFetcher";
+
+import { agent } from "@/types";
+
+import namespace from "@/static/name";
+
+const state = reactive({
+  categories: [],
+
+  selectedCategory: {},
+});
+
+onMounted(async () => {
+  const categories = await getAllCategoryRequestService();
+  const categoryInstanceArray = categories.map((cat) => {
+    const { _id, title, path, sortIndex, spot, createdAt, updatedAt } = cat;
+    const catObj = agent
+      .instanceOfName(namespace.categorySchema)
+      .createInstance(_id, title, path, sortIndex, spot, createdAt, updatedAt);
+    return catObj;
+  });
+  if (categoryInstanceArray.every((cat) => cat.ofPathCondition(cat.path))) {
+    state.categories = categoryInstanceArray;
+    state.selectedCategory = categoryInstanceArray[0];
+  } else {
+    state.categories = [];
+  }
+});
+
+// Methods
+function onCategoryItemClickEvent(clickedCategory) {
+  state.selectedCategory = clickedCategory;
+}
 </script>
-;
