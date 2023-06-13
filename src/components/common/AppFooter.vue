d80a7f85ad2c2b1e9607806e0b1183d993153124
foxmon <jjockrod@naver.com>
2023-06-13 10:57:36 +0900

    Fix: Common Layout

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-footer reveal>
+    <div :style="footerStyle">
+      <div :style="footerFontStyle">© HOBOM TECH BLOG.</div>
+      <div>관련사이트</div>
+    </div>
+  </q-footer>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+import palette from "@/utils/palette";
+
+const footerStyle = computed(() => {
+  return {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingLeft: "80px",
+    paddingRight: "80px",
+    height: "148px",
+    background: palette.colors.mainBlack,
+  };
+});
+
+const footerFontStyle = computed(() => {
+  return {
+    color: "#ffffff7a",
+    fontSize: palette.fontSize.l,
+    fontWeight: 800,
+  };
+});
+</script>
---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <q-toolbar>
+      <q-btn flat label="HBTB" :style="logoStyle" />
+      <q-space />
+      <CategoryItemList
+        :categories="props.categories"
+        @onCategoryItemClickEvent="onCategoryItemClickEvent"
+      />
+    </q-toolbar>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed } from "vue";
+
+import CategoryItemList from "../categories/CategoryItemList.vue";
+
+import palette from "@/utils/palette";
+
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  onCategoryItemClickEvent: () => true,
+});
+
+const logoStyle = computed(() => {
+  return {
+    fontSize: palette.fontSize.l,
+    fontWeight: palette.fontWeight.bold,
+  };
+});
+
+// Methods
+function onCategoryItemClickEvent(clickedCategory) {
+  emits("onCategoryItemClickEvent", clickedCategory);
+}
+</script>
