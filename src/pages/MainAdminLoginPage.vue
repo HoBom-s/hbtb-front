f62f8b0401ff5b6d26a3bc1a5df3c8172f55b15b
foxmon <jjockrod@naver.com>
2023-06-14 16:01:15 +0900

    Feat: MainAdminLoginPage

--- src/pages/MainAdminLoginPage.vue
+++ src/pages/MainAdminLoginPage.vue
@@ -1,7 +1,27 @@
 <template>
-  <q-page
-    class="window-height window-width row justify-center items-center"
-  ></q-page>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex flex-center bg-grey-2">
+        <LoginFormBox @onInputValueChangeEvent="onInputValueChangeEvent" />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+import LoginFormBox from "@/components/auth/LoginFormBox.vue";
+
+const state = reactive({
+  inputValues: {
+    email: "",
+    password: "",
+  },
+});
+
+// Methods
+function onInputValueChangeEvent(name, value) {
+  state.inputValues[name] = value;
+}
+</script>
