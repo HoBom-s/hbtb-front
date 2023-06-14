dd7d7540d9c630c6b9053ac09ae583787fe8db0d
foxmon <jjockrod@naver.com>
2023-06-14 22:42:57 +0900

    Router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import MainTechPage from "@/pages/MainTechPage.vue";
 import MainAdminLoginPage from "@/pages/MainAdminLoginPage.vue";
+import MainAdminRegisterPage from "@/pages/MainAdminRegisterPage.vue";
 
 const routes = [
   { path: "/", component: MainTechPage },
   { path: "/admin/login", component: MainAdminLoginPage },
+  { path: "/admin/register", component: MainAdminRegisterPage },
 ];
 
 export default routes;
