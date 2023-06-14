58e03f846b64965cd7cb6ce4aada8106751acd20
foxmon <jjockrod@naver.com>
2023-06-14 22:43:10 +0900

    Feat: MainAdminRegisterPage

---
+++ src/pages/MainAdminRegisterPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex flex-center bg-grey-2">
+        <RegisterFormBox
+          :inputValues="state.inputValues"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+          @onRegsiterubmitButtonClickEvent="onRegsiterubmitButtonClickEvent"
+        />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+import RegisterFormBox from "@/components/auth/RegisterFormBox.vue";
+
+import errorUtil from "@/utils/errorUtil";
+
+const state = reactive({
+  inputValues: {
+    nickname: "",
+
+    password: "",
+
+    passwordCheck: "",
+  },
+});
+
+// Methods
+function onInputValueChangeEvent(name, value) {
+  state.inputValues[name] = value;
+}
+
+function onRegsiterubmitButtonClickEvent() {
+  errorUtil.notImplemented("Login Button is not implemented");
+}
+</script>
