0344a71a3d1aa8e516f3b38c5d6d178b2047c03f
foxmon <jjockrod@naver.com>
2023-06-15 21:58:17 +0900

    Router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 import MainTechPage from "@/pages/MainTechPage.vue";
 import MainAdminLoginPage from "@/pages/MainAdminLoginPage.vue";
 import MainAdminRegisterPage from "@/pages/MainAdminRegisterPage.vue";
+import MainManagementPage from "@/pages/MainManagementPage.vue";
 
 const routes = [
   { path: "/", component: MainTechPage },
   { path: "/admin/login", component: MainAdminLoginPage },
   { path: "/admin/register", component: MainAdminRegisterPage },
+  { path: "/management", component: MainManagementPage },
 ];
 
 export default routes;
