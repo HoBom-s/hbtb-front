300ae3a4a3abd4b2ce79656d4dafc0660a589430
foxmon <jjockrod@naver.com>
2023-06-19 21:22:27 +0900

    userFetcher

--- src/apis/userFetcher.js
+++ src/apis/userFetcher.js
@@ -2,9 +2,32 @@ import axiosInstance from "./index";
 
 async function userLoginRequestService(nickname, password) {
   try {
+    const foundUser = await axiosInstance.post("/user/login", {
+      nickname: nickname,
+      password: password,
+    });
+    const { data } = foundUser;
+    return data;
+  } catch (error) {
+    const { status, message } = error;
+    throw new Error(`User login failed with ${status}, message: ${message}`);
+  }
+}
+
+async function userRegisterRequestService(
+  nickname,
+  password,
+  profileImg,
+  role,
+  introduction
+) {
+  try {
     const createdUser = await axiosInstance.post("/user/create", {
       nickname: nickname,
       password: password,
+      profileImg: profileImg,
+      role: role,
+      introduction: introduction,
     });
     const { data } = createdUser;
     return data;
@@ -14,4 +37,4 @@ async function userLoginRequestService(nickname, password) {
   }
 }
 
-export { userLoginRequestService };
+export { userLoginRequestService, userRegisterRequestService };
