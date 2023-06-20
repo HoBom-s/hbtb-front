e89664179444d44c1e31d2fcd2b2cc9792cffb84
foxmon <jjockrod@naver.com>
2023-06-20 23:05:09 +0900

    Feat: CardManagementList

---
+++ src/components/cards/CardManagementList.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card class="bg-transparent no-shadow no-border" bordered>
+    <q-card-section class="q-pa-none">
+      <div class="row q-col-gutter-sm">
+        <div
+          v-for="(cardItem, index) in props.cardItems"
+          :key="index"
+          class="col-md-3 col-sm-12 col-xs-12"
+        >
+          <CardManagement
+            :iconPosition="props.iconPosition"
+            :cardItem="cardItem"
+          />
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+import CardManagement from "@/components/cards/CardManagement.vue";
+
+const props = defineProps({
+  iconPosition: {
+    type: String,
+    required: true,
+    default: "left",
+  },
+
+  cardItems: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
