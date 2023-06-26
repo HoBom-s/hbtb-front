d623b7b0f09b43ec8642d65794f2b48ed1c7e167
foxmon <jjockrod@naver.com>
2023-06-26 22:26:33 +0900

    Feat: DataGridChart

---
+++ src/components/charts/DataGridChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-mt-md">
+    <q-table
+      flat
+      bordered
+      seperator="cell"
+      rowKey="name"
+      :title="props.title"
+      :columns="props.columns"
+      :rows="props.rows"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  columns: {
+    type: Array,
+    required: true,
+  },
+
+  rows: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
