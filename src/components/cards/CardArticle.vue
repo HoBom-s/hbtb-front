74b0ecc7d26ab8c4763801269538f21f7cb0a388
foxmon <jjockrod@naver.com>
2023-06-27 10:10:52 +0900

    Card

--- src/components/cards/CardArticle.vue
+++ src/components/cards/CardArticle.vue
@@ -2,26 +2,22 @@
   <div class="q-pb-md cursor-pointer" :style="cardStyle">
     <q-card>
       <q-card-section horizontal>
-        <q-img
-          src="https://cdn.quasar.dev/img/parallax2.jpg"
-          width="280px"
-          height="190px"
-        />
+        <q-img :src="props.item.thumbnail" width="280px" height="190px" />
         <q-card-section>
           <div :style="cardTitleFontStyle">
             {{ props.item.title }}
           </div>
           <div class="q-mt-md text-subtitle2">
-            {{ props.item.subTitle }}
+            {{ props.item.subtitle }}
           </div>
           <div class="q-mt-lg text-subtitle2">
             <q-avatar size="26px">
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
-            by {{ props.item.author }}
+            by {{ authorInformation }}
           </div>
-          <div class="text-subtitle2" :style="cardAuthorFontStyle">
-            {{ props.item.createdAt }}
+          <div class="text-subtitle2 q-mt-lg" :style="cardAuthorFontStyle">
+            {{ props.item.createdAt.split("T")[0] }}
           </div>
         </q-card-section>
       </q-card-section>
@@ -41,6 +37,11 @@ const props = defineProps({
   },
 });
 
+const authorInformation = computed(() => {
+  const toNickname = props.item.writers.map((wri) => wri.nickname).join(", ");
+  return toNickname;
+});
+
 const cardStyle = computed(() => {
   return {
     width: "700px",
@@ -50,6 +51,7 @@ const cardStyle = computed(() => {
 const cardTitleFontStyle = computed(() => {
   return {
     fontSize: palette.fontSize.md,
+    fontWeight: palette.fontWeight.bold,
   };
 });
 
