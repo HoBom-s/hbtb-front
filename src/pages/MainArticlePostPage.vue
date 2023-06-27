77902ec4d551a23b73085cf5727e1cf2943b7abd
foxmon <jjockrod@naver.com>
2023-06-27 16:26:51 +0900

    Feat: MainArticlePostPage

---
+++ src/pages/MainArticlePostPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <CommonManageLayoutContainer>
+    <q-editor
+      height="auto"
+      minHeight="768px"
+      maxHeight="100%"
+      :modelValue="state.contents"
+    />
+  </CommonManageLayoutContainer>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+import CommonManageLayoutContainer from "@/containers/CommonManageLayoutContainer.vue";
+
+const state = reactive({
+  contents: "",
+});
+</script>
