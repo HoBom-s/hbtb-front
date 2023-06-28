ae83473e2713e064824fe06c83cff5fcce3bce90
foxmon <jjockrod@naver.com>
2023-06-28 13:34:26 +0900

    Feat: BaseEditDialog

---
+++ src/components/dialog/BaseEditDialog.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-dialog :modelValue="props.isDialogOpen" persistent>
+    <q-card style="min-width: 350px">
+      <q-card-section>
+        <div
+          class="text-h6"
+          :style="{ fontWeight: palette.fontWeight.bigBold }"
+        >
+          <q-icon
+            name="warning"
+            color="warning"
+            size="1.525rem"
+            class="q-mr-xs"
+          />
+          {{ props.title }}
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+        <slot></slot>
+      </q-card-section>
+      <q-card-section align="right" class="text-primary">
+        <q-btn
+          flat
+          label="Cancel"
+          @click="onBaseEditDialogCloseButtonClickEvent"
+        />
+        <q-btn
+          flat
+          label="Confirm"
+          @click="onBaseEditDialogAddButtonClickEvent"
+        />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+import palette from "@/utils/palette";
+
+const props = defineProps({
+  isDialogOpen: {
+    type: Boolean,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  // No validation
+  onBaseEditDialogCloseButtonClickEvent: () => true,
+
+  onBaseEditDialogAddButtonClickEvent: () => true,
+});
+
+function onBaseEditDialogCloseButtonClickEvent() {
+  emits("onBaseEditDialogCloseButtonClickEvent", false);
+}
+
+function onBaseEditDialogAddButtonClickEvent() {
+  emits("onBaseEditDialogAddButtonClickEvent");
+}
+</script>
