f951b6c29cdccfb358a7d7a7c71b131189c99539
foxmon <jjockrod@naver.com>
2023-06-28 13:34:51 +0900

    AppManageSidebarItem

--- src/components/common/AppMannageSidebarItem.vue
+++ src/components/common/AppMannageSidebarItem.vue
@@ -2,7 +2,7 @@
   <q-item-section avatar>
     <q-icon :name="props.menuItem.iconName" />
   </q-item-section>
-  <q-item-section>
+  <q-item-section @click="props.menuItem.onClick">
     <q-item-label>{{ props.menuItem.menuTitle }}</q-item-label>
   </q-item-section>
 </template>
