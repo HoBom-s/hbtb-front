c07f390d5a2a16f85a9950efa41d928faa03b404
foxmon <jjockrod@naver.com>
2023-06-28 17:58:06 +0900

    BaseEditDialog

--- src/components/dialog/BaseEditDialog.vue
+++ src/components/dialog/BaseEditDialog.vue
@@ -22,12 +22,16 @@
         <q-btn
           flat
           label="Cancel"
+          :style="{ fontWeight: palette.fontWeight.bold }"
           @click="onBaseEditDialogCloseButtonClickEvent"
         />
         <q-btn
           flat
+          bordered
+          color="orange"
           label="Confirm"
-          @click="onBaseEditDialogAddButtonClickEvent"
+          :style="{ fontWeight: palette.fontWeight.bold }"
+          @click="onBaseEditDialogConfirmButtonClickEvent"
         />
       </q-card-section>
     </q-card>
@@ -55,14 +59,14 @@ const emits = defineEmits({
   // No validation
   onBaseEditDialogCloseButtonClickEvent: () => true,
 
-  onBaseEditDialogAddButtonClickEvent: () => true,
+  onBaseEditDialogConfirmButtonClickEvent: () => true,
 });
 
 function onBaseEditDialogCloseButtonClickEvent() {
   emits("onBaseEditDialogCloseButtonClickEvent", false);
 }
 
-function onBaseEditDialogAddButtonClickEvent() {
-  emits("onBaseEditDialogAddButtonClickEvent");
+function onBaseEditDialogConfirmButtonClickEvent() {
+  emits("onBaseEditDialogConfirmButtonClickEvent");
 }
 </script>
