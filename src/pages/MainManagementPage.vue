c91a0e09858bf71cb99f0caff16007beb689fce3
foxmon <jjockrod@naver.com>
2023-06-28 17:58:13 +0900

    MainManagementPage

--- src/pages/MainManagementPage.vue
+++ src/pages/MainManagementPage.vue
@@ -9,12 +9,14 @@
           :categories="state.categories"
           :tags="state.tags"
           @onTabValueChangeEvent="onTabValueChangeEvent"
+          @onItemSlideLeftEvent="onItemSlideLeftEvent"
+          @onItemSlideRightEvent="onItemSlideRightEvent"
         />
       </div>
       <div class="col">
         <div class="q-mt-md">
           <q-card :style="{ maxHeight: '450px' }">
-            <q-card-section class>
+            <q-card-section>
               <div
                 class="text-h6"
                 :style="{ fontWeight: palette.fontWeight.bigBold }"
@@ -35,36 +37,135 @@
       </div>
     </div>
     <DataGridChart :title="'ARTICLES'" :columns="columns" :rows="rows" />
+    <BaseEditDialog
+      :isDialogOpen="state.isUpdateDialogOpen"
+      :title="'UPDATE ITEM'"
+      @onBaseEditDialogCloseButtonClickEvent="
+        onBaseEditDialogCloseButtonClickEvent
+      "
+      @onBaseEditDialogConfirmButtonClickEvent="
+        onBaseEditDialogConfirmButtonClickEvent
+      "
+    >
+      <div v-if="state.selectedTabItem === 'category'">
+        <AppInput
+          :inputType="'text'"
+          :inputLabel="'Category Title'"
+          :inputName="'categoryTitle'"
+          :isMarginTop="true"
+          :inputValue="state.inputValues.categoryTitle"
+          :validateFormValue="validateCategoryTitle"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+        />
+        <AppInput
+          :inputType="'text'"
+          :inputLabel="'Category Path'"
+          :inputName="'categoryPath'"
+          :isMarginTop="true"
+          :inputValue="state.inputValues.categoryPath"
+          :validateFormValue="validateCategoryPath"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+        />
+        <AppInput
+          :inputType="'number'"
+          :inputLabel="'Category Sort Index'"
+          :inputName="'categorySortIndex'"
+          :isMarginTop="true"
+          :inputValue="state.inputValues.categorySortIndex"
+          :validateFormValue="validateCategorySortIndex"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+        />
+        <AppSelect
+          :selectLabel="'Category Spot'"
+          :options="categorySpotOptions"
+          :modelValues="state.inputValues.categorySpot"
+          :isMarginTop="true"
+          @onSelectValueChangeEvent="onSelectValueChangeEvent"
+        />
+      </div>
+      <div v-else>
+        <AppInput
+          :inputType="'text'"
+          :inputLabel="'Tag Title'"
+          :inputName="'tagTitle'"
+          :isMarginTop="true"
+          :inputValue="state.inputValues.tagTitle"
+          :validateFormValue="validateTagTitle"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+        />
+        <AppInput
+          :inputType="'text'"
+          :inputLabel="'Tag Path'"
+          :inputName="'tagPath'"
+          :isMarginTop="true"
+          :inputValue="state.inputValues.tagPath"
+          :validateFormValue="validateTagPath"
+          @onInputValueChangeEvent="onInputValueChangeEvent"
+        />
+      </div>
+    </BaseEditDialog>
+    <BaseEditDialog
+      :isDialogOpen="state.isRemoveDialogOpen"
+      :title="'WARNING'"
+      @onBaseEditDialogCloseButtonClickEvent="
+        onBaseEditDialogCloseButtonClickEvent
+      "
+      @onBaseEditDialogConfirmButtonClickEvent="
+        onBaseEditDialogConfirmButtonClickEvent
+      "
+    >
+      <div class="q-mt-md">Do you want to delete ?</div>
+    </BaseEditDialog>
   </CommonManageLayoutContainer>
 </template>
 
 <script setup>
-import { reactive, onMounted, computed } from "vue";
+import { reactive, onBeforeMount, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 
 import useStorage from "@/hooks/useStorage";
+import useValidate from "@/hooks/useValidate";
 
 import CommonManageLayoutContainer from "@/containers/CommonManageLayoutContainer.vue";
 import CardManagementList from "@/components/cards/CardManagementList.vue";
 import ManagementItemBox from "@/components/management/ManagementItemBox.vue";
 import DataGridChart from "@/components/charts/DataGridChart.vue";
 import CircleChart from "@/components/charts/CircleChart.vue";
+import BaseEditDialog from "@/components/dialog/BaseEditDialog.vue";
+import AppInput from "@/components/common/AppInput.vue";
+import AppSelect from "@/components/common/AppSelect.vue";
 
-import { getAllCategoryRequestService } from "@/apis/categoryFetcher";
-import { getAllTagRequestService } from "@/apis/tagFetcher";
+import {
+  getAllCategoryRequestService,
+  updateCategoryRequestService,
+  deleteCategoryRequestService,
+} from "@/apis/categoryFetcher";
+import {
+  getAllTagRequestService,
+  updateTagRequestService,
+  deleteTagRequestService,
+} from "@/apis/tagFetcher";
 import { getAllArticleRequestService } from "@/apis/articleFetcher";
 
 import { agent } from "@/types";
 
 import namespace from "@/static/name";
+
 import palette from "@/utils/palette";
+import funcUtil from "@/utils/funcUtil";
+import validator from "@/utils/validator";
+import errorUtil from "@/utils/errorUtil";
 
 const router = useRouter();
+
+let timer;
 
 const tabItems = [
   { name: "category", label: "Category", iconName: "list" },
   { name: "tag", label: "Tag", iconName: "tag" },
 ];
+
+const categorySpotOptions = ["Header", "Footer"];
 
 const state = reactive({
   articles: [],
@@ -74,6 +175,27 @@ const state = reactive({
   tags: [],
 
   selectedTabItem: "category",
+
+  selectedUpdateItem: {},
+
+  selectedRemoveItem: "",
+
+  isUpdateDialogOpen: false,
+
+  isRemoveDialogOpen: false,
+
+  inputValues: {
+    categoryTitle: "",
+    categoryPath: "",
+    categorySortIndex: 1,
+    categorySpot: "",
+    tagTitle: "",
+    tagPath: "",
+  },
+});
+
+onBeforeMount(() => {
+  clearTimeout(timer);
 });
 
 onMounted(async () => {
@@ -266,8 +388,189 @@ const rows = computed(() => {
   return rowArray;
 });
 
+const validateCategoryTitle = computed(() => {
+  const categoryTitleValue = state.inputValues.categoryTitle;
+  const validateObject = validator.validateCategoryTitle(categoryTitleValue);
+  return validateObject;
+});
+
+const validateCategoryPath = computed(() => {
+  const categoryPathValue = state.inputValues.categoryPath;
+  const validateObject = validator.validateCategoryPath(categoryPathValue);
+  return validateObject;
+});
+
+const validateCategorySortIndex = computed(() => {
+  const categorySortIndexValue = state.inputValues.categorySortIndex;
+  const validateObject = validator.validateCategorySortIndex(
+    categorySortIndexValue
+  );
+  return validateObject;
+});
+
+const validateTagTitle = computed(() => {
+  const tagTitleValue = state.inputValues.tagTitle;
+  const validateObject = validator.validateTagTitle(tagTitleValue);
+  return validateObject;
+});
+
+const validateTagPath = computed(() => {
+  const tagPathValue = state.inputValues.tagPath;
+  const validateObject = validator.validateTagPath(tagPathValue);
+  return validateObject;
+});
+
 // Methods
 function onTabValueChangeEvent(value) {
   state.selectedTabItem = value;
+}
+
+function onItemSlideLeftEvent(e, item) {
+  const { reset } = e;
+
+  funcUtil
+    .match(state.selectedTabItem)
+    .on(
+      (tabValue) => tabValue === "category",
+      () => {
+        const { title, path, sortIndex, spot } = item;
+        const selectedSpot = spot === "H" ? "Header" : "Footer";
+        state.inputValues.categoryTitle = title;
+        state.inputValues.categoryPath = path;
+        state.inputValues.categorySortIndex = sortIndex;
+        state.inputValues.categorySpot = selectedSpot;
+        state.selectedUpdateItem = item;
+      }
+    )
+    .on(
+      (tabValue) => tabValue === "tag",
+      () => {
+        const { title, path } = item;
+        state.inputValues.tagTitle = title;
+        state.inputValues.tagPath = path;
+      }
+    )
+    .otherwise(() => errorUtil.notImplemented());
+
+  state.selectedUpdateItem = item;
+  state.isUpdateDialogOpen = true;
+  quasarFinalReset(reset);
+}
+
+function onItemSlideRightEvent(e, _id) {
+  const { reset } = e;
+  state.selectedRemoveItem = _id;
+  state.isRemoveDialogOpen = true;
+  quasarFinalReset(reset);
+}
+
+function onInputValueChangeEvent(name, value) {
+  state.inputValues[name] = value;
+}
+
+function onSelectValueChangeEvent(value) {
+  state.inputValues.categorySpot = value;
+}
+
+function onBaseEditDialogCloseButtonClickEvent() {
+  state.selectedRemoveItem = "";
+  state.selectedUpdateItem = {};
+  if (state.isUpdateDialogOpen) {
+    state.isUpdateDialogOpen = false;
+  } else if (state.isRemoveDialogOpen) {
+    state.isRemoveDialogOpen = false;
+  }
+}
+
+async function onBaseEditDialogConfirmButtonClickEvent() {
+  if (state.isUpdateDialogOpen) {
+    const updateResult = await funcUtil
+      .match(state.selectedTabItem)
+      .on(
+        (tabValue) => tabValue === "category",
+        async () => {
+          const {
+            categoryTitle,
+            categoryPath,
+            categorySortIndex,
+            categorySpot,
+          } = state.inputValues;
+
+          const [isValidateUpdateCategory] = useValidate([
+            () =>
+              validator.validateCategoryTitle(categoryTitle).hasError === false,
+            () =>
+              validator.validateCategoryPath(categoryPath).hasError === false,
+            () =>
+              validator.validateCategorySortIndex(categorySortIndex)
+                .hasError === false,
+          ]);
+
+          if (!isValidateUpdateCategory) {
+            return null;
+          }
+          const updatedCategoryResult = await updateCategoryRequestService({
+            _id: state.selectedUpdateItem._id,
+            title: categoryTitle,
+            path: categoryPath,
+            sortIndex: categorySortIndex,
+            spot: categorySpot,
+          });
+          return updatedCategoryResult;
+        }
+      )
+      .on(
+        (tabValue) => tabValue === "tag",
+        async () => {
+          const { tagTitle, tagPath } = state.inputValues;
+
+          const [isValidateUpdateTag] = useValidate([
+            () => validator.validateTagTitle(tagTitle).hasError === false,
+            () => validator.validateTagPath(tagPath).hasError === false,
+          ]);
+
+          if (!isValidateUpdateTag) {
+            return null;
+          }
+
+          const updatedTagResult = await updateTagRequestService({
+            _id: state.selectedUpdateItem._id,
+            title: tagTitle,
+            path: tagPath,
+            count: state.selectedUpdateItem.count,
+          });
+          return updatedTagResult;
+        }
+      )
+      .otherwise(() => errorUtil.notImplemented());
+
+    if (updateResult._id) {
+      state.selectedUpdateItem = {};
+      state.isUpdateDialogOpen = false;
+    }
+  } else if (state.isRemoveDialogOpen) {
+    const removeResult = await funcUtil
+      .match(state.selectedTabItem)
+      .on(
+        (tabValue) => tabValue === "category",
+        async () => await deleteCategoryRequestService(state.selectedRemoveItem)
+      )
+      .on(
+        (tabValue) => tabValue === "tag",
+        async () => await deleteTagRequestService(state.selectedRemoveItem)
+      )
+      .otherwise(() => errorUtil.notImplemented());
+
+    if (removeResult) {
+      state.selectedRemoveItem = "";
+      state.isRemoveDialogOpen = false;
+    }
+  }
+}
+
+function quasarFinalReset(reset) {
+  timer = setTimeout(() => {
+    reset();
+  }, 500);
 }
 </script>
