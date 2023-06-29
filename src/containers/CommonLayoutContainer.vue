d0c3078500d2b4557e16669548fbc0e250e586ad
foxmon <jjockrod@naver.com>
2023-06-29 10:53:24 +0900

    Fix: CommonLayoutContainer

--- src/containers/CommonLayoutContainer.vue
+++ src/containers/CommonLayoutContainer.vue
@@ -2,8 +2,14 @@
   <q-layout>
     <AppHeader
       :categories="state.categories"
+      :tags="state.tags"
       :selectedCategory="state.selectedCategory"
+      :selectedTag="state.selectedTag"
+      :isSearchItemClick="state.isSearchItemClick"
       @onCategoryItemClickEvent="onCategoryItemClickEvent"
+      @onTagItemClickEvent="onTagItemClickEvent"
+      @onSearchItemClickEvent="onSearchItemClickEvent"
+      @onLogoButtonClickEvent="onLogoButtonClickEvent"
     />
     <div :style="mainContainerStyle"><slot></slot></div>
     <AppFooter />
@@ -23,6 +29,7 @@ import AppHeader from "../components/common/AppHeader.vue";
 import AppFooter from "../components/common/AppFooter.vue";
 
 import { getAllCategoryRequestService } from "@/apis/categoryFetcher";
+import { getAllTagRequestService } from "@/apis/tagFetcher";
 
 import { agent } from "@/types";
 
@@ -33,11 +40,18 @@ const router = useRouter();
 const state = reactive({
   categories: [],
 
+  tags: [],
+
   selectedCategory: {},
+
+  selectedTag: {},
+
+  isSearchItemClick: false,
 });
 
 onMounted(async () => {
   const categories = await getAllCategoryRequestService();
+
   const categoryInstanceArray = categories
     .map((cat) => {
       const { _id, title, path, sortIndex, spot, createdAt, updatedAt } = cat;
@@ -55,12 +69,25 @@ onMounted(async () => {
       return catObj;
     })
     .filter((cat) => cat.spot === "H");
+
   if (categoryInstanceArray.every((cat) => cat.ofPathCondition(cat.path))) {
     state.categories = categoryInstanceArray;
     state.selectedCategory = categoryInstanceArray[0];
   } else {
     state.categories = [];
   }
+
+  const tags = await getAllTagRequestService();
+
+  const tagInstanceArray = tags.map((tag) => {
+    const { _id, title, path, count, createdAt } = tag;
+    const tagObj = agent
+      .instanceOfName(namespace.tagSchema)
+      .createInstance(_id, title, path, count, createdAt);
+    return tagObj;
+  });
+
+  state.tags = tagInstanceArray;
 });
 
 const mainContainerStyle = computed(() => {
@@ -76,5 +103,17 @@ function onCategoryItemClickEvent(clickedCategory) {
   state.selectedCategory = clickedCategory;
   const path = clickedCategory.path;
   router.push(path);
+}
+
+function onTagItemClickEvent(clickedTag) {
+  state.selectedTag = clickedTag;
+}
+
+function onSearchItemClickEvent() {
+  state.isSearchItemClick = !state.isSearchItemClick;
+}
+
+function onLogoButtonClickEvent() {
+  router.push("/");
 }
 </script>
