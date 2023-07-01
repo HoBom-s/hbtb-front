cb4015012ebf3309f4672b3772b5d3f49df60c70
foxmon <jjockrod@naver.com>
2023-07-01 15:22:28 +0900

    Feat: PostOptionBox

---
+++ src/components/publish/PostOptionBox.vue
@@ -0,0 +1,89 @@
+<template>
+  <q-card flat bordered>
+    <q-card-section>
+      <div class="text-h6" :style="{ fontWeight: palette.fontWeight.bold }">
+        Article Options
+      </div>
+    </q-card-section>
+    <q-separator />
+    <q-card-section>
+      <div class="col q-row-gutter-sm">
+        <div
+          :style="{
+            fontWeight: palette.fontWeight.bold,
+            fontSize: palette.fontSize.l,
+          }"
+        >
+          Category
+        </div>
+        <div>
+          <AppSelect
+            :selectLabel="'Choose category'"
+            :options="props.categories"
+            :modelValues="props.selectedCategory"
+            :isMarginTop="true"
+            @onSelectValueChangeEvent="onSelectValueChangeEvent"
+          />
+        </div>
+        <div
+          class="q-mt-md q-mb-sm"
+          :style="{
+            fontWeight: palette.fontWeight.bold,
+            fontSize: palette.fontSize.l,
+          }"
+        >
+          Tag
+        </div>
+        <TagItemList :tags="props.tags" />
+      </div>
+    </q-card-section>
+    <q-separator />
+    <q-card-section class="q-pa-md">
+      <q-btn
+        flat
+        outlined
+        label="Publish"
+        class="bg-orange text-white fit"
+        :style="{
+          fontWeight: palette.fontWeight.bigBold,
+          fontSize: palette.fontSize.xl,
+        }"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+import AppSelect from "@/components/common/AppSelect.vue";
+import TagItemList from "@/components/tags/TagItemList.vue";
+
+import palette from "@/utils/palette";
+
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+
+  tags: {
+    type: Array,
+    required: true,
+  },
+
+  selectedCategory: {
+    type: String,
+    required: true,
+  },
+});
+
+const emits = defineEmits({
+  // No validation
+  onSelectValueChangeEvent: () => true,
+});
+
+function onSelectValueChangeEvent(value) {
+  emits("onSelectValueChangeEvent", value);
+}
+</script>
