a1353eab70e1dd96e80c26d433db476c15702f28
foxmon <jjockrod@naver.com>
2023-07-01 15:23:20 +0900

    Fix: MainArticlePostPage

--- src/pages/MainArticlePostPage.vue
+++ src/pages/MainArticlePostPage.vue
@@ -1,26 +1,126 @@
 <template>
   <CommonManageLayoutContainer>
-    <q-editor
-      height="auto"
-      minHeight="768px"
-      maxHeight="100%"
-      :modelValue="state.contents"
-      @update:modelValue="onContentChangeEvent"
+    <PostHeaderBox
+      :inputValues="state.inputValues"
+      :validateArticleTitle="validateArticleTitle"
+      :validateArticleSubtitle="validateArticleSubtitle"
+      @onInputValueChangeEvent="onInputValueChangeEvent"
     />
+    <div class="row q-mt-md q-col-gutter-md">
+      <div class="col-md-9 col-sm-12 col-xs-12">
+        <q-editor
+          height="auto"
+          minHeight="600px"
+          maxHeight="100%"
+          :modelValue="state.contents"
+          @update:modelValue="onContentChangeEvent"
+        />
+      </div>
+      <div class="col-md-3 col-sm-12 col-xs-12">
+        <PostOptionBox
+          :categories="categoryOptions"
+          :tags="state.tags"
+          :selectedCategory="state.inputValues.selectedCategory"
+          @onSelectValueChangeEvent="onSelectValueChangeEvent"
+        />
+      </div>
+    </div>
   </CommonManageLayoutContainer>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, onMounted, computed } from "vue";
 
 import CommonManageLayoutContainer from "@/containers/CommonManageLayoutContainer.vue";
+import PostHeaderBox from "@/components/publish/PostHeaderBox.vue";
+import PostOptionBox from "@/components/publish/PostOptionBox.vue";
+
+import { getAllCategoryRequestService } from "@/apis/categoryFetcher";
+import { getAllTagRequestService } from "@/apis/tagFetcher";
+
+import { agent } from "@/types";
+
+import namespace from "@/static/name";
+
+import validator from "@/utils/validator";
 
 const state = reactive({
+  categories: [],
+
+  tags: [],
+
   contents: "",
+
+  inputValues: {
+    title: "",
+    subtitle: "",
+    selectedCategory: "",
+  },
+});
+
+onMounted(async () => {
+  const categoryResults = await getAllCategoryRequestService();
+
+  const categoryInstanceArray = categoryResults.map((cat) => {
+    const { _id, title, path, sortIndex, spot, createdAt, updatedAt } = cat;
+
+    const categoryObject = agent
+      .instanceOfName(namespace.categorySchema)
+      .createInstance(_id, title, path, sortIndex, spot, createdAt, updatedAt);
+
+    return categoryObject;
+  });
+
+  const tagResults = await getAllTagRequestService();
+
+  const tagInstanceArray = tagResults.map((tag) => {
+    const { _id, title, path, count, createdAt } = tag;
+
+    const tagObject = agent
+      .instanceOfName(namespace.tagSchema)
+      .createInstance(_id, title, path, count, createdAt);
+
+    return tagObject;
+  });
+
+  state.categories = categoryInstanceArray;
+
+  state.tags = tagInstanceArray;
+
+  state.inputValues.selectedCategory = categoryInstanceArray[0].title;
+});
+
+const validateArticleTitle = computed(() => {
+  const titleValue = state.inputValues.title;
+  const validTitleObject = validator.validateArticleTitle(titleValue);
+  return validTitleObject;
+});
+
+const validateArticleSubtitle = computed(() => {
+  const subtitleValue = state.inputValues.subtitle;
+  const validSubtitleObject = validator.validateArticleSubtitle(subtitleValue);
+  return validSubtitleObject;
+});
+
+const categoryOptions = computed(() => {
+  const options = state.categories.map((cat) => {
+    const categoryTitle = cat.title;
+    return categoryTitle;
+  });
+
+  return options;
 });
 
 // Methods
 function onContentChangeEvent(changedContents) {
   console.log(changedContents);
+}
+
+function onInputValueChangeEvent(name, value) {
+  state.inputValues[name] = value;
+}
+
+function onSelectValueChangeEvent(value) {
+  state.inputValues.selectedCategory = value;
 }
 </script>
