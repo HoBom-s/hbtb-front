b8a429d9c5b6def5cc9e30c7ceffc42f1e1e0cf9
foxmon <jjockrod@naver.com>
2023-07-04 17:25:12 +0900

    main

--- src/main.js
+++ src/main.js
@@ -10,16 +10,30 @@ import vuepressTheme from "@kangc/v-md-editor/lib/theme/vuepress.js";
 import Prism from "prismjs";
 import enUS from "@kangc/v-md-editor/lib/lang/en-US";
 
+// Preview
+import VMdPreview from "@kangc/v-md-editor/lib/preview";
+import githubTheme from "@kangc/v-md-editor/lib/theme/github.js";
+
+// Highlight JavaScript
+import hljs from "highlight.js";
+
 import "@kangc/v-md-editor/lib/style/base-editor.css";
 import "@kangc/v-md-editor/lib/theme/style/vuepress.css";
+import "@kangc/v-md-editor/lib/style/preview.css";
+import "@kangc/v-md-editor/lib/theme/style/github.css";
 
 VueMarkdownEditor.lang.use("en-US", enUS);
 VueMarkdownEditor.use(vuepressTheme, {
   Prism,
 });
 
+VMdPreview.use(githubTheme, {
+  Hljs: hljs,
+});
+
 const app = createApp(App);
 app.use(router);
 app.use(Quasar, quasarUserOptions);
 app.use(VueMarkdownEditor);
+app.use(VMdPreview);
 app.mount("#app");
